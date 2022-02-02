899b7aecfe273c1b27684da4aae6dbc854760f8b
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-02 17:10:15 +0800

    Получение комнат на сервере

--- components/ConversationCard/index.tsx
+++ components/ConversationCard/index.tsx
@@ -6,27 +6,27 @@ import { Avatar } from '../Avatar'
 
 interface ConversationCardProps {
     title: string;
-    users: string[]
+    guests: string[]
     avatars: string[];
     guestsCount: number;
     speakersCount: number;
 }
 
-export const ConversationCard: React.FC<ConversationCardProps> = ({ title, users = [], avatars = [], guestsCount, speakersCount }) => {
+export const ConversationCard: React.FC<ConversationCardProps> = ({ title, guests = [], avatars = [], guestsCount, speakersCount }) => {
     return (
         <div className={clsx(WhiteBlockStyles.block, styles.card, 'mb-30')}>
             <h4 className={styles.title}>{title}</h4>
             <div className={clsx('d-flex mt-10', styles.content)}>
                 <div className={styles.avatars}>
                     {avatars.map((url, i) => (
-                        <Avatar width="55px" height='55px' src={url} key={url} className={i === avatars.length - 1 ? 'lastAvatar' : ''} />
+                        <Avatar width="55px" height='55px' src={url} key={url} className={avatars.length > 1 && i === avatars.length - 1 ? 'lastAvatar' : ''} />
                     ))}
                 </div>
                 <div className={clsx(styles.info, 'ml-10')}>
                     <ul className={styles.users}>
                         {
-                            users.map(name => (
-                                <li key={name}>
+                            guests.map((name, i) => (
+                                <li key={name + i}>
                                     {name}
                                 </li>
                             ))
--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+import React from "react";
 import { Button } from "../components/Button";
 import { ConversationCard } from "../components/ConversationCard";
 import { Header } from "../components/Header";
 
-export default function RoomsPage() {
+import Axios from '../core/axios'
+
+export default function RoomsPage({ rooms }) {
     return (
         <>
             <Header />
@@ -13,10 +17,41 @@ export default function RoomsPage() {
                         + Start room
                     </Button>
                 </div>
-                <div className="mt-20">
-                    <ConversationCard />
+                <div className="grid mt-30">
+                    {
+                        rooms.map(obj => (
+                            <Link key={obj.id} href={obj.link}>
+                                <a>
+                                    <ConversationCard
+                                        title={obj.title}
+                                        guests={obj.guests}
+                                        avatars={obj.avatars}
+                                        guestsCount={obj.guestsCount}
+                                        speakersCount={obj.speakerCount}
+                                    />
+                                </a>
+                            </Link>
+                        ))
+                    }
                 </div>
             </div>
         </>
     )
+}
+
+export const getServerSideProps = async () => {
+    try {
+        const { data } = await Axios.get('/rooms.json')
+        return {
+            props: {
+                rooms: data
+            }
+        }
+    } catch (error) {
+        return {
+            props: {
+                rooms: []
+            }
+        }
+    }
 }
