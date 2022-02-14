10e130810f6571a34fdefcbaa6c7970a59a4f599
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-02-14 16:25:47 +0800

    проверка логина на странице пользователя

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router"
 import { Header } from "../../components/Header"
 import { Profile } from "../../components/Profile"
+import { wrapper } from "../../redux/store"
+import { checkAuth } from "../../utils/checkAuth"
 
 export default function ProfilePage() {
     const router = useRouter()
@@ -14,3 +16,21 @@ export default function ProfilePage() {
         </>
     )
 }
+
+
+export const getServerSideProps = wrapper.getServerSideProps(async (ctx) => {
+    try {
+        const user = await checkAuthuth(ctx)
+        if (!user) {
+            return {
+                props: {},
+                redirect: {
+                    permanent: false,
+                    destination: '/'
+                }
+            }
+        }
+    } catch (error) {
+
+    }
+})
