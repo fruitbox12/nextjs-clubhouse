5a3065b667b8eeb382bfb17544742419c7a89101
fruitbox12 <dylanwong007@gmail.com>
2023-01-24 01:08:03 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  reactStrictMode: false,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },}
